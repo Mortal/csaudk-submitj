8581d355b890ece00934e063db6a7c25c26c83e8
Mathias Rav <rav@cs.au.dk>
2017-08-18 11:39:11 +0200

    Add Maximum.java skeleton

---
+++ Maximum.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+public class Maximum {
+    public int computeMaximum(ArrayList<Integer> input) {
+        // Implement your code here to return the maximum!
+        return 42;
+    }
+
+    public static void testAll() {
+        test1();
+        test2();
+    }
+
+    public static void test1() {
+        int[] input = { 96, 66, 82, 63, 78, 37, 100, 96, 98, 17, 13, 7, 28, 74, 73, 77, 41, 25, 53, 93 };
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i : input) list.add(i);
+
+        int output = new Maximum().computeMaximum(list);
+        int correctAnswer = 100;
+
+        if (output != correctAnswer)
+            throw new RuntimeException("Expected output " + correctAnswer +
+                                       "but got " + output);
+        else
+            System.out.println("test1: OK");
+    }
+
+    public static void test2() {
+        int[] input = { 90, 8, 38, 69, 82, 79, 67, 27, 17, 46, 78, 14, 47, 18, 36, 55, 55, 74, 12, 95 };
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i : input) list.add(i);
+
+        int output = new Maximum().computeMaximum(list);
+        int correctAnswer = 95;
+
+        if (output != correctAnswer)
+            throw new RuntimeException("Expected output " + correctAnswer +
+                                       "but got " + output);
+        else
+            System.out.println("test2: OK");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < n; ++i) list.add(sc.nextInt());
+        System.out.println(new Maximum().computeMaximum(list));
+    }
+}
