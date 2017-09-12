70f0420c687492e93b280d200e6dc939af01924a
Mathias Rav <rav@cs.au.dk>
2017-09-12 10:34:44 +0200

    Update skeleton

--- tasks/max/Maximum.java
+++ tasks/max/Maximum.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Maximum {
     public int computeMaximum(ArrayList<Integer> input) {
         // Implement your code here to return the maximum!
-        return 42;
+        return input.get(0);
     }
 
     public static void testAll() {
@@ -13,39 +13,54 @@ public class Maximum {
 
     public static void test1() {
         int[] input = { 96, 66, 82, 63, 78, 37, 100, 96, 98, 17, 13, 7, 28, 74, 73, 77, 41, 25, 53, 93 };
+        int correctAnswer = 100;
+
         ArrayList<Integer> list = new ArrayList<Integer>();
         for (int i : input) list.add(i);
 
         int output = new Maximum().computeMaximum(list);
-        int correctAnswer = 100;
 
         if (output != correctAnswer)
-            throw new RuntimeException("Expected output " + correctAnswer +
-                                       " but got " + output);
+            outputFail("test1",
+                       "Expected output " + correctAnswer +
+                       " but got " + output);
         else
-            System.out.println("test1: OK");
+            outputPass("test1");
     }
 
     public static void test2() {
         int[] input = { 90, 8, 38, 69, 82, 79, 67, 27, 17, 46, 78, 14, 47, 18, 36, 55, 55, 74, 12, 95 };
+        int correctAnswer = 95;
+
         ArrayList<Integer> list = new ArrayList<Integer>();
         for (int i : input) list.add(i);
 
         int output = new Maximum().computeMaximum(list);
-        int correctAnswer = 95;
 
         if (output != correctAnswer)
-            throw new RuntimeException("Expected output " + correctAnswer +
-                                       " but got " + output);
+            outputFail("test2",
+                       "Expected output " + correctAnswer +
+                       " but got " + output);
         else
-            System.out.println("test2: OK");
+            outputPass("test2");
+    }
+
+    private static void outputPass(String testName) {
+        System.out.println("[PASS " + testName + "]");
+    }
+
+    private static void outputFail(String testName, String message) {
+        System.err.println("[FAIL " + testName + "] " + message);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < n; ++i) list.add(sc.nextInt());
-        System.out.println(new Maximum().computeMaximum(list));
+        int testcases = sc.nextInt();
+        for (int t = 0; t < testcases; ++t) {
+            int n = sc.nextInt();
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            for (int i = 0; i < n; ++i) list.add(sc.nextInt());
+            System.out.println(new Maximum().computeMaximum(list));
+        }
     }
 }
