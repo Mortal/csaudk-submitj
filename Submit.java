Mortal/csaudk-submitj

commit 1e836632d11aa90c4f513e106c5dad1fafabc8c0

Only detect each file once in Submit.java

Submit.java
@@ -311,7 +311,7 @@ public class Submit {
     }
 
     private boolean validateFilenames() throws IOException {
-        List<String> r = new ArrayList<String>();
+        Set<String> r = new HashSet<String>();
         for (String fileName : getFilenames()) {
             try(FileReader file = new FileReader(fileName)) {
                 BufferedReader rd = new BufferedReader(file);

commit c93d7118f30150ec289b57936147f8e43f2678c9

Fix join() function after changing items
Fixes bug introduced in 1e836632d11aa90c4f513e106c5dad1fafabc8c0.

Submit.java
@@ -1,4 +1,4 @@
-// Version: 2017100301
+// Version: 2017100302
 import java.io.*;
 import java.net.*;
 import java.nio.charset.*;
@@ -299,7 +299,7 @@ public class Submit {
         return http;
     }
 
-    private static String join(String delimiter, List<String> items) {
+    private static String join(String delimiter, Iterable<String> items) {
         String s = "";
         StringBuilder b = new StringBuilder();
         for (String x : items) {