8e74da87e9ca3bf12d004d624bf615610b61263e
Mathias Rav <rav@cs.au.dk>
2017-10-30 16:04:19 +0100

    Add work-in-progress pal description and skeleton

---
+++ tasks/pal/LongestPalindromeSubsequence.java
@@ -0,0 +1,131 @@
+// HERE BE DRAGONS! Not yet ready for FADS 2017!
+import java.io.*;
+import java.util.*;
+public class LongestPalindromeSubsequence {
+    public ArrayList<Character> longestPalindromeSubsequence(ArrayList<Character> input) {
+        ArrayList<Character> result = new ArrayList<>();
+
+        // Implement your code here to compute
+        // the longest palindrome subsequence of the input!
+
+        return result;
+    }
+
+    public static void testAll() {
+        clearTerminal();
+        test1();
+        test2();
+        test3();
+        test4();
+    }
+
+    public static void test1() {
+        char[] input = { 'b', 'c', 'a', 'b', 'c', 'a', 'b', 'a' };
+        char[] correctAnswer = { 'b', 'a', 'b', 'a', 'b' };
+
+        try {
+            checkPal("test1", input, correctAnswer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail("test1", "Exception: " + e);
+            return;
+        }
+    }
+
+    public static void test2() {
+        char[] input = { 'a', 'i', 'b', 'o', 'h', 'p', 'h', 'o', 'b', 'i', 'a' };
+        char[] correctAnswer = { 'a', 'i', 'b', 'o', 'h', 'p', 'h', 'o', 'b', 'i', 'a' };
+
+        try {
+            checkPal("test2", input, correctAnswer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail("test2", "Exception: " + e);
+            return;
+        }
+    }
+
+    public static void test3() {
+        char[] input = { 'r', 'h', 'o', 't', 'a', 't', 'h', 'o', 'r', 'y' };
+        char[] correctAnswer = { 'r', 'o', 't', 'a', 't', 'o', 'r' };
+
+        try {
+            checkPal("test3", input, correctAnswer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail("test3", "Exception: " + e);
+            return;
+        }
+    }
+
+    public static void test4() {
+        char[] input = { 'r', 'a', 'h', 'a', 'n', 'n', 'a', 'h' };
+        char[] correctAnswer = { 'h', 'a', 'n', 'n', 'a', 'h' };
+
+        try {
+            checkPal("test4", input, correctAnswer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail("test4", "Exception: " + e);
+            return;
+        }
+    }
+
+    private static void checkPal(String testName, char[] input, char[] correctAnswer) {
+        ArrayList<Character> list = new ArrayList<>();
+        for (char c : input) list.add(c);
+
+        ArrayList<Character> answer = (
+            new LongestPalindromeSubsequence().longestPalindromeSubsequence(list));
+
+        if (answer.size() != correctAnswer.length) {
+            outputFail(testName,
+                       "Expected palindrome of length " + correctAnswer.length + ", got " + answer);
+            return;
+        }
+
+        int j = 0;
+        for (int i : input) {
+            if (j < answer.size() && answer.get(j) == i) j++;
+        }
+        if (j < answer.size()) {
+            outputFail(testName, "Not a subsequence of the input: " + answer);
+            return;
+        }
+
+        for (int i = 0; i < answer.size()-1-i; i++)
+            if (answer.get(i) != answer.get(answer.size()-1-i)) {
+                outputFail(testName, "Not a palindrome: " + answer);
+                return;
+            }
+
+        outputPass(testName);
+    }
+
+    private static void clearTerminal() {
+        System.out.print('\u000C');
+    }
+
+    private static void outputPass(String testName) {
+        System.out.println("[Pass " + testName + "]");
+    }
+
+    private static void outputFail(String testName, String message) {
+        System.out.println("[FAIL " + testName + "] " + message);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testcases = sc.nextInt();
+        for (int t = 0; t < testcases; ++t) {
+            int n = sc.nextInt();
+            ArrayList<Character> list = new ArrayList<>();
+            for (int i = 0; i < n; ++i) list.add(sc.next().charAt(0));
+            LongestPalindromeSubsequence lis = new LongestPalindromeSubsequence();
+            ArrayList<Character> pal = lis.longestPalindromeSubsequence(list);
+            System.out.println(pal.size());
+            for (int i = 0; i < pal.size(); i++)
+                System.out.println(pal.get(i));
+        }
+    }
+}
---
+++ tasks/pal/README.md
@@ -0,0 +1,56 @@
+Longest palindrome subsequence
+==============================
+
+**Here be dragons! This exercise description is not yet ready for FADS 2017.**
+
+**Task ID: `"pal"`**
+
+A string is a palindrome if the string is equal to its reversal.
+
+In this problem you are given a string `T[1..n]` of length *n*,
+and your task is to compute the longest subsequence of T which is a palindrome.
+
+Let L(i, j) denote the length of the longest palindrome subsequence of `T[i..j]`.
+
+**Example:** For the string `T = bcabcaba` of length 8, L(3, 8) = 5
+since the string `T[3..8] = abcaba` contains the palindrome `abcba` as a subsequence.
+
+L(i, j) can be determined from the following recursion, where 1 ≤ i ≤ n and 1 ≤ j ≤ n:
+
+```
+          ⎧             0              if j < i
+          ⎪
+          ⎪             1              if i = j
+L(i, j) = ⎨
+          ⎪      1 + L(i+1, j-1)       if i < j and a_i = a_j
+          ⎪
+          ⎩ max(L(i+1, j), L(i, j-1))  if i < j and a_i ≠ a_j
+```
+
+In this problem you should implement an algorithm based on dynamic programming
+to compute the longest palindrome subsequence in a list of *n* numbers.
+
+**Concretely,** you must implement a public method named
+`longestPalindromeSubsequence` that takes an `ArrayList<Character> input` as argument
+and returns an `ArrayList<Character>`.
+
+Use the skeleton
+<a href="https://github.com/Mortal/csaudk-submitj/raw/master/tasks/lis/LongestPalindromeSubsequence.java">
+`LongestPalindromeSubsequence.java`</a>
+and the
+<a href="https://github.com/Mortal/csaudk-submitj/raw/master/Submit.java">
+`Submit` program</a>
+inside BlueJ (right click and choose "Save link as").
+
+**Input constraints:**
+
+  * 1 ≤ N ≤ 50 000
+
+**Scoring:**
+
+  * 1 point for correct and fast algorithm
+
+An O(N²) solution based on the above dynamic programming formula is fast enough to get the point.
+
+If you are looking for a challenge, then you may try to come up with an
+O(N log N) solution for this problem!
