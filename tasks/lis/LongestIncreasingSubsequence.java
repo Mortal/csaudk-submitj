69bcb7362953fbb8d0b8e690fb6942bbdc23d7e7
Mathias Rav <rav@cs.au.dk>
2017-10-30 14:05:00 +0100

    Add work-in-progress LIS

---
+++ tasks/lis/LongestIncreasingSubsequence.java
@@ -0,0 +1,153 @@
+// HERE BE DRAGONS! Not yet ready for FADS 2017!
+import java.io.*;
+import java.util.*;
+public class LongestIncreasingSubsequence {
+    public ArrayList<Integer> longestIncreasingSubsequence(ArrayList<Integer> input) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        // Implement your code here to compute
+        // the longest increasing subsequence of the input!
+        // You can use the following code to insert a dummy "-infinity" element:
+        //input.add(0, Integer.MIN_VALUE);
+        // Remember to not include -infinity in the result list!
+
+        // After backtracking, you can use Collections.reverse()
+        // to reverse the result in O(n) time:
+        //Collections.reverse(result);
+
+        return result;
+    }
+
+    public static void testAll() {
+        clearTerminal();
+        test1();
+        test2();
+        test3();
+        test4();
+        test5();
+    }
+
+    public static void test1() {
+        int[] input = { 4, 8, 11, 14 };
+        int[] correctAnswer = { 4, 8, 11, 14 };
+
+        try {
+            checkLis("test1", input, correctAnswer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail("test1", "Exception: " + e);
+            return;
+        }
+    }
+
+    public static void test2() {
+        int[] input = { 12, 8, 11, 14 };
+        int[] correctAnswer = { 8, 11, 14 };
+
+        try {
+            checkLis("test2", input, correctAnswer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail("test2", "Exception: " + e);
+            return;
+        }
+    }
+
+    public static void test3() {
+        int[] input = { 1, 8, 11, 7 };
+        int[] correctAnswer = { 1, 8, 11 };
+
+        try {
+            checkLis("test3", input, correctAnswer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail("test3", "Exception: " + e);
+            return;
+        }
+    }
+
+    public static void test4() {
+        int[] input = { 14, 8, 11, 13, 7 };
+        int[] correctAnswer = { 8, 11, 13 };
+
+        try {
+            checkLis("test4", input, correctAnswer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail("test4", "Exception: " + e);
+            return;
+        }
+    }
+
+    public static void test5() {
+        int[] input = { 95, 66, 82, 63, 78, 37, 100, 96, 98, 17, 13, 7, 28, 74, 73, 77, 41, 25, 53, 93 };
+        int[] correctAnswer = { 13, 28, 41, 53, 93 };
+
+        try {
+            checkLis("test5", input, correctAnswer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail("test5", "Exception: " + e);
+            return;
+        }
+    }
+
+    private static void checkLis(String testName, int[] input, int[] correctAnswer) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i : input) list.add(i);
+
+        ArrayList<Integer> answer = (
+            new LongestIncreasingSubsequence().longestIncreasingSubsequence(list));
+
+        if (answer.size() != correctAnswer.length) {
+            outputFail(testName,
+                       "Expected list of length " + correctAnswer.length + ", got " + answer);
+            return;
+        }
+
+        int j = 0;
+        for (int i : input) {
+            if (j < answer.size() && answer.get(j) == i) j++;
+        }
+        if (j < answer.size()) {
+            outputFail(testName, "Not a subsequence of the input: " + answer);
+            return;
+        }
+
+        for (int i = 1; i < answer.size(); i++)
+            if (answer.get(i-1) >= answer.get(i)) {
+                outputFail(testName, "Not increasing: " +
+                           answer.get(i-1) + " >= " + answer.get(i) + " in " + answer);
+                return;
+            }
+
+        outputPass(testName);
+    }
+
+    private static void clearTerminal() {
+        System.out.print('\u000C');
+    }
+
+    private static void outputPass(String testName) {
+        System.out.println("[Pass " + testName + "]");
+    }
+
+    private static void outputFail(String testName, String message) {
+        System.out.println("[FAIL " + testName + "] " + message);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testcases = sc.nextInt();
+        for (int t = 0; t < testcases; ++t) {
+            int n = sc.nextInt();
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            for (int i = 0; i < n; ++i) list.add(sc.nextInt());
+            LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
+            ArrayList<Integer> backtrack = lis.longestIncreasingSubsequence(list);
+            System.out.println(backtrack.size());
+            for (int i = 0; i < backtrack.size(); i++)
+                System.out.println(backtrack.get(i));
+        }
+    }
+}
---
+++ tasks/lis/README.md
@@ -0,0 +1,48 @@
+Longest increasing subsequence
+==============================
+
+**Here be dragons! This exercise description is not yet ready for FADS 2017.**
+
+**Task ID: `"lis"`**
+
+Given a list of *n* numbers `a_1`, ..., `a_n`,
+we let L(i) denote the longest increasing subsequence of `a_1`, ..., `a_i`
+where `a_i` is the last element in the subsequence.
+
+L(i) can be determined from the following recursion, where 0 ≤ i ≤ n,
+and we set `a_0 = -∞` to make the formula valid when j = 0:
+
+```
+       ⎧                       0                     if i = 0
+L(i) = ⎨
+       ⎩ 1 + max { L(j) | 0 ≤ j < i and a_j < a_i }  if i > 0
+```
+
+In this problem you should implement an algorithm based on dynamic programming
+to compute the longest increasing subsequence in a list of *n* numbers.
+
+**Concretely,** you must implement a public method named
+`longestIncreasingSubsequence` that takes an `ArrayList<Integer> input` as argument
+and returns an `ArrayList<Integer>`.
+
+Use the skeleton
+<a href="https://github.com/Mortal/csaudk-submitj/raw/master/tasks/lis/LongestIncreasingSubsequence.java">
+`LongestIncreasingSubsequence.java`</a>
+and the
+<a href="https://github.com/Mortal/csaudk-submitj/raw/master/Submit.java">
+`Submit` program</a>
+inside BlueJ (right click and choose "Save link as").
+
+**Input constraints:**
+
+  * 1 ≤ N ≤ 1 000 000
+  * Each element is between -1 000 000 000 and 1 000 000 000
+
+**Scoring:**
+
+  * 1 point for correct and fast algorithm
+
+An O(N²) solution based on the above dynamic programming formula is fast enough to get the point.
+
+If you are looking for a challenge, then you may try to come up with an
+O(N log N) solution for this problem!
