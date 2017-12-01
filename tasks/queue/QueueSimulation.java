c35600c7a96104cd24df99af3019960927d52715
Mathias Rav <rav@cs.au.dk>
2017-12-01 13:42:17 +0100

    Refactor QueueSimulation

--- tasks/queue/QueueSimulation.java
+++ tasks/queue/QueueSimulation.java
@@ -20,55 +20,40 @@ public class QueueSimulation {
     public static void test1() {
         int[] input = { 1, 2, 3, 4, 5 };
         int correctAnswer = 2;
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i : input) list.add(i);
-
-        int output;
-        try {
-            output = new QueueSimulation().simulate(list);
-        } catch (Exception e) {
-            outputFail("test1",
-                       "Exception: " + e);
-            return;
-        }
-
-        if (output != correctAnswer)
-            outputFail("test1",
-                       "Expected output " + correctAnswer +
-                       " but got " + output);
-        else
-            outputPass("test1");
+        runTest("test1", input, correctAnswer);
     }
 
     public static void test2() {
         int[] input = { 123 };
         int correctAnswer = 123;
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i : input) list.add(i);
-
-        int output;
-        try {
-            output = new QueueSimulation().simulate(list);
-        } catch (Exception e) {
-            outputFail("test2",
-                       "Exception: " + e);
-            return;
-        }
-
-        if (output != correctAnswer)
-            outputFail("test2",
-                       "Expected output " + correctAnswer +
-                       " but got " + output);
-        else
-            outputPass("test2");
+        runTest("test2", input, correctAnswer);
     }
 
     public static void test3() {
         int[] input = { 4, 3, 2, 1 };
         int correctAnswer = 1;
+        runTest("test3", input, correctAnswer);
+    }
 
+    public static void test1024() {
+        int N = 1024;
+        int correctAnswer = 1024;
+        runTestMany("test1024", N, correctAnswer);
+    }
+
+    public static void test513() {
+        int N = 513;
+        int correctAnswer = 2;
+        runTestMany("test513", N, correctAnswer);
+    }
+
+    public static void test100() {
+        int N = 100;
+        int correctAnswer = 72;
+        runTestMany("test100", N, correctAnswer);
+    }
+
+    private static void runTest(String testName, int[] input, int correctAnswer) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         for (int i : input) list.add(i);
 
@@ -76,89 +61,24 @@ public class QueueSimulation {
         try {
             output = new QueueSimulation().simulate(list);
         } catch (Exception e) {
-            outputFail("test3",
+            outputFail(testName,
                        "Exception: " + e);
             return;
         }
 
         if (output != correctAnswer)
-            outputFail("test3",
+            outputFail(testName,
                        "Expected output " + correctAnswer +
                        " but got " + output);
         else
-            outputPass("test3");
+            outputPass(testName);
     }
 
-    public static void test1024() {
-        int N = 1024;
-        int correctAnswer = 1024;
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 1; i <= N; i++) list.add(i);
-
-        int output;
-        try {
-            output = new QueueSimulation().simulate(list);
-        } catch (Exception e) {
-            outputFail("test1024",
-                       "Exception: " + e);
-            return;
-        }
-
-        if (output != correctAnswer)
-            outputFail("test1024",
-                       "Expected output " + correctAnswer +
-                       " but got " + output);
-        else
-            outputPass("test1024");
-    }
-
-    public static void test513() {
-        int N = 513;
-        int correctAnswer = 2;
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 1; i <= N; i++) list.add(i);
-
-        int output;
-        try {
-            output = new QueueSimulation().simulate(list);
-        } catch (Exception e) {
-            outputFail("test513",
-                       "Exception: " + e);
-            return;
-        }
-
-        if (output != correctAnswer)
-            outputFail("test513",
-                       "Expected output " + correctAnswer +
-                       " but got " + output);
-        else
-            outputPass("test513");
-    }
-
-    public static void test100() {
-        int N = 100;
-        int correctAnswer = 72;
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 1; i <= N; i++) list.add(i);
-
-        int output;
-        try {
-            output = new QueueSimulation().simulate(list);
-        } catch (Exception e) {
-            outputFail("test100",
-                       "Exception: " + e);
-            return;
-        }
-
-        if (output != correctAnswer)
-            outputFail("test100",
-                       "Expected output " + correctAnswer +
-                       " but got " + output);
-        else
-            outputPass("test100");
+    private static void runTestMany(String testName, int N, int correctAnswer) {
+        // Run test on input = { 1, 2, 3, ..., N }
+        int[] input = new int[N];
+        for (int i = 0; i < n; i++) input[i] = i + 1;
+        runTest(testName, input, correctAnswer);
     }
 
     private static void clearTerminal() {
