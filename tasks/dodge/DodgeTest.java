9a6e75af89f08d25a798448297003746291dc376
Mathias Rav <rav@cs.au.dk>
2017-12-01 13:15:53 +0100

    Refactor DodgeTest.java

--- tasks/dodge/DodgeTest.java
+++ tasks/dodge/DodgeTest.java
@@ -17,193 +17,76 @@ public class DodgeTest {
     public static void test1() {
         int[] input = { -1, -7, -10, -20, 10, 18, 5, -11, 8, 1, 8, 11, 18 };
         int[] correctAnswers = { 0, 2, 2, 3, 0, 0, 0, 0 };
-
-        Dodgeball d = new Dodgeball();
-        int j = 0;
-        for (int i : input) {
-            if (i < 0) d.addPlayer(-i);
-            else {
-                int output = d.throwBall(i);
-                int correctAnswer = correctAnswers[j];
-                j++;
-
-                if (output != correctAnswer) {
-                    outputFail("test1",
-                               "Expected output " + correctAnswer +
-                               " but got " + output);
-                    return;
-                }
-            }
-        }
-        outputPass("test1");
+        runTest("test1", input, correctAnswers);
     }
 
     public static void test2() {
         int[] input = { -3, -4, -6, 3, 5, 6, 6 };
         int[] correctAnswers = { 0, 1, 0, 1 };
-
-        Dodgeball d = new Dodgeball();
-        int j = 0;
-        for (int i : input) {
-            if (i < 0) d.addPlayer(-i);
-            else {
-                int output = d.throwBall(i);
-                int correctAnswer = correctAnswers[j];
-                j++;
-
-                if (output != correctAnswer) {
-                    outputFail("test2",
-                               "Expected output " + correctAnswer +
-                               " but got " + output);
-                    return;
-                }
-            }
-        }
-        outputPass("test2");
+        runTest("test2", input, correctAnswers);
     }
 
     public static void test3() {
         int[] input = { -2, 3, -1, 6, -5, 2 };
         int[] correctAnswers = { 1, 3, 1 };
-
-        Dodgeball d = new Dodgeball();
-        int j = 0;
-        for (int i : input) {
-            if (i < 0) d.addPlayer(-i);
-            else {
-                int output = d.throwBall(i);
-                int correctAnswer = correctAnswers[j];
-                j++;
-
-                if (output != correctAnswer) {
-                    outputFail("test3",
-                               "Expected output " + correctAnswer +
-                               " but got " + output);
-                    return;
-                }
-            }
-        }
-        outputPass("test3");
+        runTest("test3", input, correctAnswers);
     }
 
     public static void test4() {
         int[] input = { -4, 3, -1, 2, 3 };
         int[] correctAnswers = { 1, 1, 0 };
-
-        Dodgeball d = new Dodgeball();
-        int j = 0;
-        for (int i : input) {
-            if (i < 0) d.addPlayer(-i);
-            else {
-                int output = d.throwBall(i);
-                int correctAnswer = correctAnswers[j];
-                j++;
-
-                if (output != correctAnswer) {
-                    outputFail("test4",
-                               "Expected output " + correctAnswer +
-                               " but got " + output);
-                    return;
-                }
-            }
-        }
-        outputPass("test4");
+        runTest("test4", input, correctAnswers);
     }
 
     public static void test5() {
         int[] input = { -50000000, 1};
         int[] correctAnswers = { 49999999 };
-
-        Dodgeball d = new Dodgeball();
-        int j = 0;
-        for (int i : input) {
-            if (i < 0) d.addPlayer(-i);
-            else {
-                int output = d.throwBall(i);
-                int correctAnswer = correctAnswers[j];
-                j++;
-
-                if (output != correctAnswer) {
-                    outputFail("test5",
-                               "Expected output " + correctAnswer +
-                               " but got " + output);
-                    return;
-                }
-            }
-        }
-        outputPass("test5");
+        runTest("test5", input, correctAnswers);
     }
 
     public static void test6() {
         int[] input = { -3000000, 5000000, -2000001, 1 };
         int[] correctAnswers = { 2000000, 2000000 };
-
-        Dodgeball d = new Dodgeball();
-        int j = 0;
-        for (int i : input) {
-            if (i < 0) d.addPlayer(-i);
-            else {
-                int output = d.throwBall(i);
-                int correctAnswer = correctAnswers[j];
-                j++;
-
-                if (output != correctAnswer) {
-                    outputFail("test6",
-                               "Expected output " + correctAnswer +
-                               " but got " + output);
-                    return;
-                }
-            }
-        }
-        outputPass("test6");
+        runTest("test6", input, correctAnswers);
     }
 
     public static void test7() {
         int[] input = { -4, -3, -2, 1, 1, 1, 1, 1, 1 };
         int[] correctAnswers = { 1, 0, 2, 0, 3, 0 };
-
-        Dodgeball d = new Dodgeball();
-        int j = 0;
-        for (int i : input) {
-            if (i < 0) d.addPlayer(-i);
-            else {
-                int output = d.throwBall(i);
-                int correctAnswer = correctAnswers[j];
-                j++;
-
-                if (output != correctAnswer) {
-                    outputFail("test7",
-                               "Expected output " + correctAnswer +
-                               " but got " + output);
-                    return;
-                }
-            }
-        }
-        outputPass("test7");
+        runTest("test7", input, correctAnswers);
     }
 
     public static void test8() {
         int[] input = { -5, -4, -3, 1, 1, 1, 1, 1, 1 };
         int[] correctAnswers = { 2, 0, 3, 0, 4, 0 };
+        runTest("test8", input, correctAnswers);
+    }
 
-        Dodgeball d = new Dodgeball();
-        int j = 0;
-        for (int i : input) {
-            if (i < 0) d.addPlayer(-i);
-            else {
-                int output = d.throwBall(i);
-                int correctAnswer = correctAnswers[j];
-                j++;
+    public static void runTest(String testName, int[] input, int[] correctAnswers) {
+        try {
+            Dodgeball d = new Dodgeball();
+            int j = 0;
+            for (int i : input) {
+                if (i < 0) d.addPlayer(-i);
+                else {
+                    int output = d.throwBall(i);
+                    int correctAnswer = correctAnswers[j];
+                    j++;
 
-                if (output != correctAnswer) {
-                    outputFail("test8",
-                               "Expected output " + correctAnswer +
-                               " but got " + output);
-                    return;
+                    if (output != correctAnswer) {
+                        outputFail(testName,
+                                   "Expected output " + correctAnswer +
+                                   " but got " + output);
+                        return;
+                    }
                 }
             }
+            outputPass(testName);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail(testName,
+                       "Exception: " + e);
         }
-        outputPass("test8");
     }
 
     private static void clearTerminal() {
