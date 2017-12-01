21e52f26e491af31dc00e968f5e5c20cce778a06
Mathias Rav <rav@cs.au.dk>
2017-12-01 13:29:09 +0100

    Refactor LongestIncreasingSubsequence.java

--- tasks/lis/LongestIncreasingSubsequence.java
+++ tasks/lis/LongestIncreasingSubsequence.java
@@ -33,113 +33,63 @@ public class LongestIncreasingSubsequence {
     public static void test1() {
         int[] input = { 4, 8, 11, 14 };
         int[] correctAnswer = { 4, 8, 11, 14 };
-
-        try {
-            checkLis("test1", input, correctAnswer);
-        } catch (Exception e) {
-            e.printStackTrace();
-            outputFail("test1", "Exception: " + e);
-            return;
-        }
+        runTest("test1", input, correctAnswer);
     }
 
     public static void test2() {
         int[] input = { 12, 8, 11, 14 };
         int[] correctAnswer = { 8, 11, 14 };
-
-        try {
-            checkLis("test2", input, correctAnswer);
-        } catch (Exception e) {
-            e.printStackTrace();
-            outputFail("test2", "Exception: " + e);
-            return;
-        }
+        runTest("test2", input, correctAnswer);
     }
 
     public static void test3() {
         int[] input = { 1, 8, 11, 7 };
         int[] correctAnswer = { 1, 8, 11 };
-
-        try {
-            checkLis("test3", input, correctAnswer);
-        } catch (Exception e) {
-            e.printStackTrace();
-            outputFail("test3", "Exception: " + e);
-            return;
-        }
+        runTest("test3", input, correctAnswer);
     }
 
     public static void test4() {
         int[] input = { 14, 8, 11, 13, 7 };
         int[] correctAnswer = { 8, 11, 13 };
-
-        try {
-            checkLis("test4", input, correctAnswer);
-        } catch (Exception e) {
-            e.printStackTrace();
-            outputFail("test4", "Exception: " + e);
-            return;
-        }
+        runTest("test4", input, correctAnswer);
     }
 
     public static void test5() {
         int[] input = { 95, 66, 82, 63, 78, 37, 100, 96, 98, 17, 13, 7, 28, 74, 73, 77, 41, 25, 53, 93 };
         int[] correctAnswer = { 13, 28, 41, 53, 93 };
-
-        try {
-            checkLis("test5", input, correctAnswer);
-        } catch (Exception e) {
-            e.printStackTrace();
-            outputFail("test5", "Exception: " + e);
-            return;
-        }
+        runTest("test5", input, correctAnswer);
     }
 
     public static void test6() {
         int[] input = { 2, 3, 1 };
         int[] correctAnswer = { 2, 3 };
-
-        try {
-            checkLis("test6", input, correctAnswer);
-        } catch (Exception e) {
-            e.printStackTrace();
-            outputFail("test6", "Exception: " + e);
-            return;
-        }
+        runTest("test6", input, correctAnswer);
     }
 
     public static void test7() {
         int[] input = { 1, 4, 2, 3 };
         int[] correctAnswer = { 1, 2, 3 };
-
-        try {
-            checkLis("test7", input, correctAnswer);
-        } catch (Exception e) {
-            e.printStackTrace();
-            outputFail("test7", "Exception: " + e);
-            return;
-        }
+        runTest("test7", input, correctAnswer);
     }
 
     public static void test8() {
         int[] input = { 3, 4, 5, 1, 2, 6 };
         int[] correctAnswer = { 3, 4, 5, 6 };
-
-        try {
-            checkLis("test8", input, correctAnswer);
-        } catch (Exception e) {
-            e.printStackTrace();
-            outputFail("test8", "Exception: " + e);
-            return;
-        }
+        runTest("test8", input, correctAnswer);
     }
 
-    private static void checkLis(String testName, int[] input, int[] correctAnswer) {
+    private static void runTest(String testName, int[] input, int[] correctAnswer) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         for (int i : input) list.add(i);
 
-        ArrayList<Integer> answer = (
-            new LongestIncreasingSubsequence().longestIncreasingSubsequence(list));
+        ArrayList<Integer> answer;
+        try {
+            answer = new LongestIncreasingSubsequence().longestIncreasingSubsequence(list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            outputFail(testName, "Exception: " + e);
+            return;
+        }
 
         if (answer.size() != correctAnswer.length) {
             outputFail(testName,
